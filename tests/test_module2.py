7aeb90a3b1ee252b70f5872e5200f0c321d76e7a
getahunTiruneh <gechtiru@gmail.com>
2024-08-13 21:40:55 +0300

    add unittest

--- tests/test_module2.py
+++ tests/test_module2.py
@@ -4,5 +4,5 @@ from scripts.module2 import add
 class Testmodule2(unittest.TestCase):
     def test_add(self):
         result = add(2, 3)
-        expected = 7
+        expected = 5
         self.assertEqual(result,expected)
